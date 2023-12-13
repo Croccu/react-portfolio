815bd049400e31aa7f83b8c9536eb46c642c189f
rico paum <ricsens@me.com>
2023-12-14 01:23:41 +0200

    About me done

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from 'react';
 import "./about.css";
+import ME from '../../assets/profile_square.png';
+import { BsAward } from "react-icons/bs";
+import { LiaLaptopCodeSolid } from "react-icons/lia";
+
+
 
 const About = () => {
   return (
-    <section id="about">About</section>
+    <section id="about">
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+
+      <div className='container about__container'>
+        <div className='about__me'>
+          <div className='about__me-image'>
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+
+        <div className='about__content'>
+          <div className='about__cards'>
+            <article className='about__card'>
+              <BsAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>3+ years working</small>
+            </article>
+            <article className='about__card'>
+              <LiaLaptopCodeSolid className='about__icon'/>
+              <h5>Projects</h5>
+              <small>2</small>
+            </article>
+          </div>
+
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam necessitatibus ducimus veritatis error eligendi, aliquam, soluta odit laborum blanditiis accusantium repellendus architecto ex asperiores voluptates obcaecati magni labore corporis repudiandae?</p>
+          <a href="#contact" className='btn btn-primary'> Contact me</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './Cta.jsx'
-import ME from "../../assets/profile.jpg";
+import ME from "../../assets/profile_sil.png";
 import HeaderSocials from './HeaderSocials.jsx';
 
 const Header = () => {
