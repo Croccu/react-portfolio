babe57635464fb106000eaf508f877f5339f7755
rico paum <ricsens@me.com>
2023-12-13 23:17:13 +0200

    Nav done

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useState } from "react";
 import './nav.css'
+import { GoHome } from "react-icons/go";
+import { AiOutlineUser } from "react-icons/ai";
+import { PiLaptop } from "react-icons/pi";
+import { TiMessages } from "react-icons/ti";
+
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState("#");
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><GoHome/></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><PiLaptop/></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><TiMessages/></a>
+    </nav>
   )
 }
 
