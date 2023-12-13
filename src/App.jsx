f9e3ab4fb59e0853d8e1641804936cf5d938e7da
rico paum <ricsens@me.com>
2023-12-13 21:34:42 +0200

    setup and first components

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Header from './components/header/Header.jsx'
+import Nav from './components/nav/Nav.jsx'
+import About from './components/about/About.jsx'
+import Experience from './components/experience/Experience.jsx'
+import Portfolio from './components/portfolio/Portfolio.jsx'
+import Contact from './components/contact/Contact.jsx'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Portfolio />
+      <Contact />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './header.css'
+import CTA from './Cta.jsx'
+import ME from "../../assets/profile.jpg";
+import HeaderSocials from './HeaderSocials.jsx';
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container header_container">
+        <h5>Hello I'm</h5>
+        <h1>Rico Paum</h1>
+        <h5 className="text-light" >Fullstack developer</h5>
+        <CTA />
+        <HeaderSocials />
+
+        <div className='me'>
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className='scroll_down'>Scroll Down</a>
+      </div>
+    </header>
+  )
+}
+
+export default Header
