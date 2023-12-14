4fd4bf766e08b207a9dbdc8efbdb4b09f5e989b7
rico paum <ricsens@me.com>
2023-12-14 21:29:20 +0200

    Experience section done

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,77 @@
 import React from 'react'
 import './experience.css'
+import { PiSealCheckFill } from "react-icons/pi";
+
 
 const Experience = () => {
   return (
-    <section id="experience">Experience</section>
+    <section id="experience">
+      <h5>What skills I have</h5>
+      <h2>My experience</h2>
+
+      <div className='container experience__container'>
+        <div className='experience__frontend'>
+          <h3>Frontend Development</h3>
+          <div className='experience__content'>
+            <article className='experience__details'>
+              <PiSealCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <PiSealCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>CSS</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <PiSealCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>JavaScript</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <PiSealCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>Bootstrap</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <PiSealCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>React</h4>
+                <small className='text-light'>Basic</small>
+              </div>
+            </article>
+          </div>
+        </div>
+
+        <div className='experience__backend'>
+          <h3>Backend Development</h3>
+          <div className='experience__content'>
+            <article className='experience__details'>
+              <PiSealCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>Ruby(Rails)</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <PiSealCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>PostgreSQL</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
